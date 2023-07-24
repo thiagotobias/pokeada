6731dc44bf9da8ad0cdfedec9f3dbcf01fb7d0fd
Rodolfo Ferreira <rodolfo.lima.ferreira11@gmail.com>
2023-07-24 13:25:10 -0300

    first commit

---
+++ src/main/java/tech/ada/pokeada/PokeadaApplication.java
@@ -0,0 +1,13 @@
+package tech.ada.pokeada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeadaApplication.class, args);
+	}
+
+}
